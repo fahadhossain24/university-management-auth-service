98a4aba7b3e3c60a182b8c88a1c43e5802dfc0fd
fahadhossain24 <fahadhossaim24@gmail.com>
2023-12-23 21:53:58 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
+const port = 4000
 
 // global middleware
 app.use(cors())
@@ -9,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: any) => {
-    res.send('server running')
+  res.send('server running')
 })
 
 export default app
