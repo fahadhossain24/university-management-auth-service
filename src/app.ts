8be30123b818d8ea50068d870dfcaf8abcd869fd
fahadhossain24 <fahadhossaim24@gmail.com>
2023-12-23 21:54:49 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,13 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
-const port = 4000
 
 // global middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('server running')
 })
 
